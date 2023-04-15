544765f0830ba3bf37f72c2e62b20d0ecb06b5dd
mayvid0 <divbillowria@gmail.com>
2023-04-16 00:08:57 +0530

    added mouse trailer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const track = document.getElementById("image-track"); //to know the current position of mouse pointer
+const trailer=document.getElementById("trailer");
 
 //invisible slider
 window.onmousedown = (e) => {
@@ -11,6 +12,21 @@ window.onmouseup = () => {
 };
 
 window.onmousemove = (e) => {
+
+  //mouse movement effect
+  const x= e.clientX -trailer.offsetWidth/2,
+        y= e.clientY - trailer.offsetHeight/2;
+
+  const keyframes ={
+    transform :`translate(${x}px ,${y}px)`
+  }
+  trailer.animate(keyframes,{
+      duration : 800 ,fill:"forwards" //retains the state after animation
+  });
+
+
+  
+
   if (track.dataset.mouseDownAt === "0") return;
 
   const mouseCurrDistance = parseFloat(track.dataset.mouseDownAt) - e.clientX,
@@ -19,7 +35,6 @@ window.onmousemove = (e) => {
   const percent = -1 * (mouseCurrDistance / mouseMaxDistance) * 100;
 
   const nextPercent = parseFloat(track.dataset.prevPercent) + percent;
-  
 
   for (const image of track.getElementsByClassName("image")) {
     image.style.objectPosition = `${nextPercent + 100}% 50%`;
@@ -41,3 +56,7 @@ window.onmousemove = (e) => {
     { duration: 1200, fill: "forwards" }
   );
 };
+
+
+
+
