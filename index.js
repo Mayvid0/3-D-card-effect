fed22494b386c088fcc4b2178d030f1ce367f053
mayvid0 <divbillowria@gmail.com>
2023-04-15 18:04:21 +0530

    added 3-D effect

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-const track = document.getElementById("image-track")  //to know the current position of mouse pointer
-
+const track = document.getElementById("image-track"); //to know the current position of mouse pointer
 
 //invisible slider
-window.onmousedown=e =>{
+window.onmousedown = (e) => {
+  track.dataset.mouseDownAt = e.clientX;
+};
 
-    track.dataset.mouseDownAt = e.clientX;
-}
+window.onmouseup = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercent = track.dataset.percent;
+};
 
-window.onmouseup =() =>{
-    track.dataset.mouseDownAt= "0";
-    track.dataset.prevPercent= track.dataset.percent;
+window.onmousemove = (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
 
-}
+  const mouseCurrDistance = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    mouseMaxDistance = window.innerWidth / 2;
 
-window.onmousemove =e =>{
+  const percent = -1 * (mouseCurrDistance / mouseMaxDistance) * 100;
 
-    if(track.dataset.mouseDownAt==="0")  return ;
+  const nextPercent = parseFloat(track.dataset.prevPercent) + percent;
+  // math.min(nextPercent,0);
+  // math.max(nextPercent,-100);
 
-    const mouseCurrDistance = parseFloat(track.dataset.mouseDownAt)-e.clientX;
-        mouseMaxDistance= window.innerWidth /2;
-    
-    const percent= (mouseCurrDistance/mouseMaxDistance) * (-100) ;
-        Math.min(nextPercent,0);
-        Math.max(nextPercent,-100);
-       const nextPercent = parseFloat(track.dataset.prevPercent) + percent;
-        
-        for(const image of track.getElementsByClassName("image")){
-            image.style.objectPosition = `${nextPercent} 50%`;
-        }
-    
-    track.dataset.percent= nextPercent;
-    
-    track.animate({
-        transform:`translate(${nextPercent}%,-50%)`
-    },{duration:1200 ,fill:"forwards"});
+  for (const image of track.getElementsByClassName("image")) {
+    image.style.objectPosition = `${nextPercent + 100}% 50%`;
+  }
 
-    image.animate({
-        objectPosition: `${nextPercent+100}% center`
-    },{duration:1200, fill:"forwards" });
-}
+  track.dataset.percent = nextPercent;
 
+  track.animate(
+    {
+      transform: `translate(${nextPercent}%,-50%)`,
+    },
+    { duration: 1200, fill: "forwards" }
+  );
+
+  image.animate(
+    {
+      objectPosition: `${nextPercent + 100}% center`,
+    },
+    { duration: 1200, fill: "forwards" }
+  );
+};
